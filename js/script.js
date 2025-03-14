6e4a546c12008722b4056fd3d4ef52b90dce5c9f
Nayronvini <Nayronvini@gmail.com>
2025-03-14 01:25:43 -0300

    feat: Adicionando página de download do jogo funcional

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ function toggleMenu() {
     let menu = document.querySelector(".menu");
     menu.classList.toggle("open");
 }
+
+document.querySelector(".btn-download").addEventListener("click", function() {
+    alert("Seu download começou! Obrigado por testar o jogo!");
+});
